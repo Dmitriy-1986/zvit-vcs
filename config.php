33dadfb22c2f45a7e678278564d13e954706b31a
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2021-10-29 21:58:46 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 try {
-	//$pdo = new PDO('mysql:dbname=onavt_28931418_test; host=sql205.hostronavt.ru', 'onavt_28931418', '54321');
-	$pdo = new PDO('mysql:dbname=onavt_28931418_zvitvchs; host=sql205.hostronavt.ru', 'onavt_28931418', '54321');
+	
+	$pdo = new PDO('mysql:dbname=zvitvchs; host=localhost', 'root', '');
 } catch (PDOException $e) {
 	die($e->getMessage());
 }
